0cff38883c086344f919ac01724740060539d7bb
Marek Wywiał <onjinx@gmail.com>
2017-08-25 10:30:46 +0200

    add ipdb/pdb macros

--- keyboards/ergodox_ez/keymaps/mine/keymap.c
+++ keyboards/ergodox_ez/keymaps/mine/keymap.c
@@ -15,6 +15,13 @@
 #define L_MEDI 3
 #define L_EMOT 4
 #define L_MIRR 5
+#define L_MACR 6
+
+
+// macros
+#define M_VERSION M(0)
+#define M_PDB M(71)
+#define M_IPDB M(72)
 
 
 
@@ -29,17 +36,60 @@ enum custom_keycodes {
 // http://configure.ergodox-ez.com/keyboard_layouts/qmabvw/edit
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-  // qwerty
-  [L_BASE] = KEYMAP(KC_ESCAPE,KC_1,KC_2,KC_3,KC_4,KC_5,KC_6,KC_TAB,KC_Q,KC_W,KC_E,KC_R,KC_T,TO(L_COLE),KC_GRAVE,KC_A,KC_S,KC_D,KC_F,KC_G,KC_LSHIFT,CTL_T(KC_Z),KC_X,KC_C,KC_V,KC_B,ALL_T(KC_NO),KC_LCTL,MO(L_MIRR),KC_LALT,KC_LGUI,KC_RALT,KC_ESCAPE,KC_INSERT,KC_HOME,KC_SPACE,KC_BSPACE,KC_END,KC_TRANSPARENT,KC_7,KC_8,KC_9,KC_0,KC_MINUS,KC_EQUAL,TO(L_SYMB),KC_Y,KC_U,KC_I,KC_O,KC_P,KC_BSLASH,KC_H,KC_J,KC_K,KC_L,LT(L_MEDI,KC_SCOLON),KC_QUOTE,MEH_T(KC_NO),KC_N,KC_M,KC_COMMA,KC_DOT,CTL_T(KC_SLASH),KC_RSHIFT,KC_RALT,MO(L_EMOT),KC_TRANSPARENT,MO(L_MIRR),MO(L_SYMB),KC_DELETE,KC_ESCAPE,KC_PGUP,KC_PGDOWN,KC_TAB,KC_ENTER),
+/* Keymap 0: Base layer ((idea))
+ *
+ * ,--------------------------------------------------.           ,--------------------------------------------------.
+ * | ESC    |   1  |   2  |   3  |   4  |   5  | SfLt |           | SfRt |   6  |   7  |   8  |   9  |   0  | BkSp   |
+ * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
+ * | Tab    |   Q  |   W  |   E  |   R  |   T  |  L1  |           |  L1  |   Y  |   U  |   I  |   O  |   P  | Del    |
+ * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
+ * | Caps/L2|   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |   ;  |Enter/L2|
+ * |--------+------+------+------+------+------|  L6  |           |  L6  |------+------+------+------+------+--------|
+ * | LSft/( |   Z  |   X  |   C  | V/L3 | B/L4 |      |           |      | N/L4 | M/L3 |   ,  |   .  |   /  | RSft/) |
+ * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
+ *   |Ctrl/[| Alt/]|   #  | Left |Right |                                       |  Up  | Down |   -  | Alt/[|Ctrl/]|
+ *   `----------------------------------'                                       `----------------------------------'
+ *                                        ,-------------.       ,-------------.
+ *                                        |  L2  | lead |       | lead |  Ins |
+ *                                 ,------|------|------|       |------+------+------.
+ *                                 | Space| BkSp | Home |       | PgUp | Enter|Space |
+ *                                 |  /   |  /   |------|       |------|   /  |  /   |
+ *                                 | Ctrl | Alt  |End/L5|       |PDn/L5|  Alt | Ctrl |
+ *                                 `--------------------'       `--------------------'
+ */
+  [L_BASE] = KEYMAP(
+      // left hand
+      KC_ESCAPE, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, 
+      KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, TO(L_COLE), 
+      KC_GRAVE, KC_A, KC_S, KC_D, KC_F, KC_G, 
+      KC_LSHIFT, CTL_T(KC_Z), KC_X, KC_C, KC_V, KC_B, ALL_T(KC_NO), 
+      KC_LCTL, MO(L_MIRR), KC_LALT, KC_LGUI, KC_RALT, 
+      // left thumb
+      KC_ESCAPE, KC_INSERT, KC_HOME, KC_SPC, KC_BSPC, KC_END,
+
+      // right hand
+      M_VERSION, KC_7, KC_8, KC_9, KC_0, KC_MINUS, KC_EQUAL, 
+      TO(L_SYMB), KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSLASH, 
+      KC_H, KC_J, KC_K, KC_L, LT(L_MEDI, KC_SCOLON), KC_QUOTE, 
+      MEH_T(KC_NO), KC_N, KC_M, KC_COMMA, KC_DOT, CTL_T(KC_SLASH), KC_RSHIFT, 
+      KC_RALT, MO(L_EMOT), KC_TRANSPARENT, MO(L_MIRR), MO(L_SYMB), 
+      // right thumb
+      KC_DELETE, KC_ESCAPE, KC_PGUP, KC_PGDOWN, KC_ENT, LT(L_MACR, KC_SPC)
+    ),
 
   // colemak
   [L_COLE] = KEYMAP(KC_ESCAPE,KC_1,KC_2,KC_3,KC_4,KC_5,KC_6,KC_TAB,KC_Q,KC_W,KC_F,KC_P,KC_G,TO(L_MEDI),KC_GRAVE,KC_A,KC_R,KC_S,KC_T,KC_D,KC_LSHIFT,CTL_T(KC_Z),KC_X,KC_C,KC_V,KC_B,ALL_T(KC_NO),KC_LCTL,MO(L_MIRR),KC_LALT,KC_LGUI,KC_RALT,KC_ESCAPE,KC_INSERT,KC_HOME,KC_SPACE,KC_BSPACE,KC_END,KC_TRANSPARENT,KC_7,KC_8,KC_9,KC_0,KC_MINUS,KC_EQUAL,TO(0),KC_J,KC_L,KC_U,KC_Y,KC_SCOLON,KC_BSLASH,KC_H,KC_N,KC_E,KC_I,KC_O,KC_QUOTE,MEH_T(KC_NO),KC_K,KC_M,KC_COMMA,KC_DOT,CTL_T(KC_SLASH),KC_RSHIFT,KC_RALT,MO(L_EMOT),KC_TRANSPARENT,MO(L_MIRR),MO(L_SYMB),KC_DELETE,KC_ESCAPE,KC_PGUP,KC_PGDOWN,KC_TAB,KC_ENTER),
 
   // symbols
-  [L_SYMB] = KEYMAP(KC_ESCAPE,KC_F1,KC_F2,KC_F3,KC_F4,KC_F5,KC_F6,KC_TRANSPARENT,KC_EXLM,KC_AT,KC_LBRACKET,KC_RBRACKET,KC_PIPE,TO(0),KC_TRANSPARENT,KC_HASH,KC_DLR,KC_LPRN,KC_RPRN,KC_GRAVE,KC_TRANSPARENT,KC_PERC,KC_CIRC,KC_LCBR,KC_RCBR,KC_TILD,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,RGB_MOD,KC_TRANSPARENT,KC_TRANSPARENT,RGB_VAD,RGB_VAI,KC_TRANSPARENT,KC_TRANSPARENT,KC_F7,KC_F8,KC_F9,KC_F10,KC_F11,KC_F12,TO(L_MEDI),KC_UP,KC_7,KC_8,KC_9,KC_ASTR,KC_F12,KC_DOWN,KC_4,KC_5,KC_6,KC_PLUS,KC_TRANSPARENT,KC_SYSTEM_POWER,KC_AMPR,KC_1,KC_2,KC_3,KC_BSLASH,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_0,KC_EQUAL,KC_TRANSPARENT,RGB_TOG,RGB_SLD,KC_TRANSPARENT,KC_TRANSPARENT,RGB_HUD,RGB_HUI),
+  [L_SYMB] = KEYMAP(
+      KC_ESCAPE,KC_F1,KC_F2,KC_F3,KC_F4,KC_F5,KC_F6,
+      KC_TRANSPARENT,KC_EXLM,KC_AT,KC_LBRACKET,KC_RBRACKET,KC_PIPE,TO(0),
+      KC_TRANSPARENT,KC_HASH,KC_DLR,KC_LPRN,KC_RPRN,KC_GRAVE,KC_TRANSPARENT,
+      KC_PERC,KC_CIRC,KC_LCBR,KC_RCBR,KC_TILD,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,RGB_MOD,KC_TRANSPARENT,KC_TRANSPARENT,RGB_VAD,RGB_VAI,KC_TRANSPARENT,KC_TRANSPARENT,KC_F7,KC_F8,KC_F9,KC_F10,KC_F11,KC_F12,TO(L_MEDI),KC_UP,KC_7,KC_8,KC_9,KC_ASTR,KC_F12,KC_DOWN,KC_4,KC_5,KC_6,KC_PLUS,KC_TRANSPARENT,KC_SYSTEM_POWER,KC_AMPR,KC_1,KC_2,KC_3,KC_BSLASH,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_0,KC_EQUAL,KC_TRANSPARENT,RGB_TOG,RGB_SLD,KC_TRANSPARENT,KC_TRANSPARENT,RGB_HUD,RGB_HUI),
 
   // media
-  [L_MEDI] = KEYMAP(KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_MS_WH_UP,KC_MS_UP,KC_MS_WH_DOWN,KC_TRANSPARENT,TO(L_SYMB),KC_TRANSPARENT,KC_TRANSPARENT,KC_MS_LEFT,KC_MS_DOWN,KC_MS_RIGHT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_MS_BTN1,KC_MS_BTN2,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,TO(L_COLE),KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_LEFT,KC_DOWN,KC_UP,KC_RIGHT,KC_TRANSPARENT,KC_MEDIA_PLAY_PAUSE,KC_TRANSPARENT,KC_TRANSPARENT,KC_MEDIA_PLAY_PAUSE,KC_MEDIA_PREV_TRACK,KC_MEDIA_NEXT_TRACK,KC_TRANSPARENT,KC_TRANSPARENT,KC_AUDIO_VOL_UP,KC_AUDIO_VOL_DOWN,KC_AUDIO_MUTE,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_WWW_BACK),
+  [L_MEDI] = KEYMAP(
+      KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_MS_WH_UP,KC_MS_UP,KC_MS_WH_DOWN,KC_TRANSPARENT,TO(L_SYMB),KC_TRANSPARENT,KC_TRANSPARENT,KC_MS_LEFT,KC_MS_DOWN,KC_MS_RIGHT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_MS_BTN1,KC_MS_BTN2,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,TO(L_COLE),KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_LEFT,KC_DOWN,KC_UP,KC_RIGHT,KC_TRANSPARENT,KC_MEDIA_PLAY_PAUSE,KC_TRANSPARENT,KC_TRANSPARENT,KC_MEDIA_PLAY_PAUSE,KC_MEDIA_PREV_TRACK,KC_MEDIA_NEXT_TRACK,KC_TRANSPARENT,KC_TRANSPARENT,KC_AUDIO_VOL_UP,KC_AUDIO_VOL_DOWN,KC_AUDIO_MUTE,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_WWW_BACK),
 
   // emotes
   [L_EMOT] = KEYMAP(KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_COLN,KC_RPRN,KC_LPRN,KC_SLASH,KC_P,KC_D,KC_TRANSPARENT,KC_SCOLON,KC_RPRN,KC_LPRN,KC_SLASH,KC_P,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_D,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT),
@@ -47,7 +97,80 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // qwerty mirror
   [L_MIRR] = KEYMAP(KC_EQUAL,KC_MINUS,KC_0,KC_9,KC_8,KC_7,KC_TRANSPARENT,KC_BSLASH,KC_P,KC_O,KC_I,KC_U,KC_Y,KC_TRANSPARENT,KC_QUOTE,KC_SCOLON,KC_L,KC_K,KC_J,KC_H,KC_LSHIFT,KC_SLASH,KC_DOT,KC_COMMA,KC_M,KC_N,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_ESCAPE,KC_DELETE,KC_PGUP,KC_ENTER,KC_TAB,KC_PGDOWN,KC_6,KC_5,KC_4,KC_3,KC_2,KC_1,KC_ESCAPE,KC_TRANSPARENT,KC_T,KC_R,KC_E,KC_W,KC_Q,KC_TAB,KC_G,KC_F,KC_D,KC_S,KC_A,KC_GRAVE,KC_TRANSPARENT,KC_B,KC_V,KC_C,KC_X,KC_Z,KC_LSHIFT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_INSERT,KC_ESCAPE,KC_BSPACE,KC_END,KC_BSPACE,KC_SPACE),
 
+/* Keymap MACR: macros
+ *
+ *
+ * ,---------------------------------------------------.           ,--------------------------------------------------.
+ * |         |      |      |      |      |      | empb |           | emnb | emfs | emfb |      |      |      |        |
+ * |---------+------+------+------+------+------+------|           |------+------+------+------+------+------+--------|
+ * |         | emtr |      |      |      |      |      |           |      | emun | emre | w-up |      |      |        |
+ * |---------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
+ * |         |      | emwr |      |      |      |------|           |------|      |w-left|w-down|w-rght|      |        |
+ * |---------+------+------+------+------+------|  ##  |           |  ##  |------+------+------+------+------+--------|
+ * |         |      |      |      |      |      |      |           |      |      |      |w-down|      |      |        |
+ * `---------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
+ *   |       |      |      |      |      |                                       |      |      |      |      |      |
+ *   `-----------------------------------'                                       `----------------------------------'
+ *                                        ,-------------.       ,-------------.
+ *                                        |      |      |       |      |      |
+ *                                 ,------|------|------|       |------+------+------.
+ *                                 |      |      |      |       |      |      |      |
+ *                                 |      |      |------|       |------|      |      |
+ *                                 |      |      |      |       |      |      |      |
+ *                                 `--------------------'       `--------------------'
+ */
+  [L_MACR] = KEYMAP(
+      // left hand
+      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+      KC_TRNS, KC_TRNS, KC_TRNS, M_PDB, KC_TRNS, KC_TRNS, 
+      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+      // left thumb
+      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+
+      // right hand
+      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+      KC_TRNS, KC_TRNS, KC_TRNS, M_IPDB, KC_TRNS, KC_TRNS, KC_TRNS,
+      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+      // right thumb
+      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+    ),
+
 };
+
+enum next_key_down_up {
+    NK_DOWN_UP,
+    NK_DOWN,
+    NK_UP // a bit of a hack, this works as long as NK_UP < KC_A
+};
+
+void send_keystrokes(uint8_t key, ...)
+{
+    va_list vl;
+    va_start(vl, key);
+    enum next_key_down_up nkdu = NK_DOWN_UP;
+    while (key != KC_NO) {
+        if (key < KC_A) {
+            nkdu = key;
+        } else {
+            switch (nkdu) {
+            case NK_DOWN_UP:
+                register_code(key);
+            case NK_UP:
+                unregister_code(key);
+                break;
+            case NK_DOWN:
+                register_code(key);
+            }
+            nkdu = NK_DOWN_UP;
+        }
+        key = va_arg(vl, int);
+    }
+    va_end(vl);
+}
 
 const uint16_t PROGMEM fn_actions[] = {
   [1] = ACTION_LAYER_TAP_TOGGLE(1)
@@ -58,10 +181,20 @@ const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
 {
       switch(id) {
         case 0:
-        if (record->event.pressed) {
-          SEND_STRING (QMK_KEYBOARD "/" QMK_KEYMAP " @ " QMK_VERSION);
-        }
-        break;
+          if (record->event.pressed) {
+            SEND_STRING (QMK_KEYBOARD "/" QMK_KEYMAP " @ " QMK_VERSION);
+          }
+          break;
+        case 71:
+          if (record->event.pressed) {
+              SEND_STRING("import pdb; pdb.set_trace()");
+          }
+          break;
+        case 72:
+          if (record->event.pressed) {
+              SEND_STRING("import ipdb; ipdb.set_trace()");
+          }
+          break;
       }
     return MACRO_NONE;
 };
@@ -92,7 +225,17 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   return true;
 }
 
+LEADER_EXTERNS();
+
 void matrix_scan_user(void) {
+    LEADER_DICTIONARY() {
+        leading = false;
+        leader_end();
+
+        SEQ_TWO_KEYS(KC_J, KC_K) {
+            send_keystrokes(KC_ESCAPE, KC_NO);
+        }
+      }
 
     uint8_t layer = biton32(layer_state);
 
