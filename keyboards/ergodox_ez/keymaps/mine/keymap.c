307a7d845faf27135214fe2d9381351831013afb
Marek Wywiał <onjinx@gmail.com>
2020-10-05 10:23:52 +0200

    feat(numpad): put 0 close to 1 ( replace with numpad dot )

--- keyboards/ergodox_ez/keymaps/mine/keymap.c
+++ keyboards/ergodox_ez/keymaps/mine/keymap.c
@@ -154,13 +154,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_NO,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
         KC_NO,   KC_PSLS, KC_P7,   KC_P8,   KC_P9,   KC_PAST, KC_F12,
                  KC_PMNS, KC_P4,   KC_P5,   KC_P6,   KC_PPLS, KC_NO,
-        KC_NO,   KC_PDOT, KC_P1,   KC_P2,   KC_P3,   KC_PEQL, KC_NO,
+        KC_NO,   KC_P0,   KC_P1,   KC_P2,   KC_P3,   KC_PEQL, KC_NO,
                           KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
 
         // right thumb
         KC_NO,   KC_NO,
                           KC_NO,
-        KC_NO,   KC_PENT, KC_P0
+        KC_NO,   KC_PENT, KC_PDOT
     ),
 
     // media
