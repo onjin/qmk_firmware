dea627ceec9722c8ea3f52f9971462aac6fb3cb5
Marek Wywiał <onjinx@gmail.com>
2022-05-10 10:19:52 +0200

    remove TAPPING_FORCE_HOLD to allow auto-repeat keys (mostly backspace :) )

--- keyboards/ergodox_ez/keymaps/onjin/config.h
+++ keyboards/ergodox_ez/keymaps/onjin/config.h
@@ -27,7 +27,7 @@
 #define IGNORE_MOD_TAP_INTERRUPT
 
 // Enable rapid switch from tap to hold, disables double tap hold auto-repeat.
-#define TAPPING_FORCE_HOLD
+// #define TAPPING_FORCE_HOLD
 
 // Recommended for heavy chording.
 #define QMK_KEYS_PER_SCAN 4
