31d3fe0b1378b529eb00ad6f8e3065d4b02e333f
Marek Wywiał <onjinx@gmail.com>
2024-04-28 21:58:37 +0200

    lead: add docker/lorem shortcuts

--- keyboards/ergodox_ez/keymaps/onjin/keymap.c
+++ keyboards/ergodox_ez/keymaps/onjin/keymap.c
@@ -100,9 +100,31 @@ void leader_end_user(void) {
 
     did_leader_succeed = false;
 
-    if (leader_sequence_one_key(KC_F)) {
-        // Leader, f => Types the below string
-        SEND_STRING("$(fzf)");
+    if (leader_sequence_one_key(KC_H)) {
+        SEND_STRING("echo  '\
+            Leader key sequences:\n\
+            \n\
+              LEAD d u       -> docker compose up\n\
+              LEAD d l       -> docker compose logs\n\
+              LEAD d s       -> docker compose stop\n\
+              LEAD d s a     -> docker stop ALL\n\
+            \n\
+              LEAD l i       -> Lorem Ipsum paragraph\n\
+              LEAD l b       -> Lorem Back To The Future paragraph\n\
+        '");
+
+    } else if(leader_sequence_two_keys(KC_D, KC_L)) {
+        SEND_STRING("docker compose logs");
+    } else if(leader_sequence_two_keys(KC_D, KC_U)) {
+        SEND_STRING("docker compose up");
+    } else if(leader_sequence_two_keys(KC_D, KC_S)) {
+        SEND_STRING("docker compose stop");
+    } else if(leader_sequence_three_keys(KC_D, KC_S, KC_A)) {
+        SEND_STRING("docker stop $(docker ps -q)");
+    } else if(leader_sequence_two_keys(KC_L, KC_I)) {
+        SEND_STRING("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas rhoncus pharetra dolor, at egestas nisi varius et. Ut nulla massa, dictum eu nisi bibendum, viverra lacinia turpis. Curabitur a mattis mauris. Pellentesque eu felis ac sem porttitor rutrum. Duis vitae purus arcu. Cras elementum quam id dapibus finibus. Duis tempor magna a arcu efficitur, interdum pellentesque dui gravida. Donec est lorem, eleifend eu elementum vel, sagittis vitae massa. Integer tincidunt est vitae interdum fermentum. Suspendisse ullamcorper mi neque, sit amet congue libero ornare rutrum. Aenean porta massa non leo porttitor dignissim. Sed dictum at dolor eget lacinia.");
+    } else if(leader_sequence_two_keys(KC_L, KC_B)) {
+        SEND_STRING("Calm down, Marty, I didn’t disintegrate anything. The molecular structure of Einstein and the car are completely intact. Marty you gotta come back with me. Of course, from a group of Libyan Nationalists. They wanted me to build them a bomb, so I took their plutonium and in turn gave them a shiny bomb case full of used pinball machine parts. Yoo. Yoo.");
     } else {
         did_leader_succeed = false;
     }
